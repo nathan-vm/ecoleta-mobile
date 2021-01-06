ac1e4e87244cad47097e9266a4178a912d255788
Nathan Vieira <nathanvieira@lett.digital>
2021-01-06 15:52:53 -0300

    Created home page with routes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import AppLoading from 'expo-app-loading'
 
-export default function App() {
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+
+import { Routes } from './src/routes'
+
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <Routes />
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
